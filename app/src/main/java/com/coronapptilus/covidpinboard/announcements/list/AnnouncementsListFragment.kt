9e876ab04a9b4c630edaf83df1267842b1f9422b
Marcos Blanco <mbbprojects2020@gmail.com>
2020-04-08 19:00:51 +0200

    Remove unnecessary code

--- app/src/main/java/com/coronapptilus/covidpinboard/announcements/list/AnnouncementsListFragment.kt
+++ app/src/main/java/com/coronapptilus/covidpinboard/announcements/list/AnnouncementsListFragment.kt
@@ -1,29 +1,7 @@
 package com.coronapptilus.covidpinboard.announcements.list
 
-import android.os.Bundle
-import android.util.Log
-import android.view.View
 import androidx.fragment.app.Fragment
 import com.coronapptilus.covidpinboard.R
-import com.coronapptilus.covidpinboard.domain.usecases.AddFavoriteUseCase
-import com.coronapptilus.covidpinboard.domain.usecases.GetFavoritesUseCase
-import org.koin.android.ext.android.inject
 
 class AnnouncementsListFragment: Fragment(R.layout.fragment_announcement_list), AnnouncementsListContract.View {
-
-    private val addFavoriteUseCase: AddFavoriteUseCase by inject()
-    private val getFavoritesUseCase: GetFavoritesUseCase by inject()
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        addFavoriteUseCase.execute(1)
-        addFavoriteUseCase.execute(2)
-        addFavoriteUseCase.execute(3)
-        val favorites = getFavoritesUseCase.execute()
-
-        favorites.favorites.forEach {
-            Log.i("favorites", it.toString())
-        }
-    }
 }
