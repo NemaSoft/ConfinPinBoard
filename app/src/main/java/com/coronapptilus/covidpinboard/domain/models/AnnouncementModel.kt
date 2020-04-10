a53d84563c18df52c72762d1408d3dae9e60e8fa
Maria MP <maria.mphob@gmail.com>
2020-04-10 10:58:38 +0200

    Add. Family target

--- app/src/main/java/com/coronapptilus/covidpinboard/domain/models/AnnouncementModel.kt
+++ app/src/main/java/com/coronapptilus/covidpinboard/domain/models/AnnouncementModel.kt
@@ -12,25 +12,26 @@ data class AnnouncementModel(
 ) {
 
     sealed class Category(val type: Int) {
-        object Sport: Category(1)
-        object Cook: Category(2)
-        object Music: Category(3)
-        object Dance: Category(4)
-        object Theater: Category(5)
-        object Literature: Category(6)
-        object Cinema: Category(7)
-        object Conference: Category(8)
-        object Interview: Category(9)
-        object WorkShop: Category(10)
-        object Formation: Category(11)
-        object Donation: Category(12)
-        object Crafts: Category(13)
-        object StoryTeller: Category(14)
-        object Others: Category(15)
+        object Sport : Category(1)
+        object Cook : Category(2)
+        object Music : Category(3)
+        object Dance : Category(4)
+        object Theater : Category(5)
+        object Literature : Category(6)
+        object Cinema : Category(7)
+        object Conference : Category(8)
+        object Interview : Category(9)
+        object WorkShop : Category(10)
+        object Formation : Category(11)
+        object Donation : Category(12)
+        object Crafts : Category(13)
+        object StoryTeller : Category(14)
+        object Others : Category(15)
     }
 
     sealed class Target(val type: Int) {
-        object Adults: Target(1)
-        object Children: Target(2)
+        object Adults : Target(1)
+        object Children : Target(2)
+        object Family : Target(3)
     }
 }
